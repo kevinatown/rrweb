e4b987c27c4e133c3d0515d1ecac618a18d7e605
Kevin <kevinatown@gmail.com>
2025-09-24 21:19:07 -0400

    mutations

--- packages/rrweb/src/record/mutation.ts
+++ packages/rrweb/src/record/mutation.ts
@@ -258,20 +258,25 @@ export default class MutationBuffer {
 
   public processMutations = (mutations: mutationRecord[]) => {
     if (mutations.length < 10_000) {
-      mutations.forEach(this.processMutation); // adds mutations to the buffer
+      window.requestIdleCallback(() => {
+        mutations.forEach(this.processMutation); // adds mutations to the buffer
+      });
     } else {
       console.log('mutations.length', mutations.length);
       while (mutations.length) {
         console.log('mutations.length', mutations.length);
         const batch = mutations.splice(0, 10_000);
-        new Promise(() => {
+        window.requestIdleCallback(() => {
+          console.log('idle callback');
           batch.forEach(this.processMutation);
         });
       }
       console.log('done batching');
     }
 
-    this.emit(); // clears buffer if not locked/frozen
+    window.requestIdleCallback(() => {
+      this.emit(); // clears buffer if not locked/frozen
+    });
   };
 
   public emit = () => {
